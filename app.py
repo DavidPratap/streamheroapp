48612c125099cd9f0511eb8bff5778594f2d8d6d
David Pratap <david.pratap@greatlearning.in>
2023-05-28 21:27:37 +0530

    new model

--- app.py
+++ app.py
@@ -10,9 +10,10 @@ from tensorflow.keras.preprocessing import image
 from PIL import Image
 st.set_option('deprecation.showPyplotGlobalUse', False)
 st.title("Dog and Cat Classifier using TensorFlow and Keras")
-model='cats_dogs_small_2.h5'
+
 
 # Step1: Load the keras model and the image
+model='cats_dogs_small_4.h5'
 model=load_model(model)
 
 #uploaded_file=st.file_uploader("Upload a cat or dog photo")
@@ -23,59 +24,26 @@ if uploaded_file is not None:
 else:
     file_name = "image1.jpg"
 
-# if uploaded_file is not None:
-#     file_path = uploaded_file.name
-#     st.write(file_path)
-# else:
-#     file_path = None
-# if uploaded_file is not None:
-#     file_path=uploaded_file.name
-#     st.write("Filename: ", file_path)
-# else:
-#     file_path=None
-# from tempfile import NamedTemporaryFile
-
-# uploaded_file = st.file_uploader("File upload", type=['jpg', 'jpeg'])
-# with NamedTemporaryFile(dir='.', suffix='.jpg') as f:
-#     f.write(uploaded_file.getbuffer())
-#     #your_function_which_takes_a_path(f.name)
-#     my_image=image.load_img(f.name, target_size=(150, 150))
-#     my_img_arr=image.img_to_array(my_image)
-#     if st.checkbox("Display Image", False):
-#         image=Image.open(uploaded_file)
-#     st.image(image)
-#     my_img_arr=np.expand_dims(my_img_arr, axis=0)
 
 
-# Step2 : get the file path 
-# if uploaded_file:
-#    st.write("Filename: ", uploaded_file.name)
-
-# for file in uploaded_file:
-#     file_path_temp=uploaded_file.name
-# image_folder_path=st.text_input("Input your folder path where images are stored")
-# st.write(image_folder_path)
+# Step2 : get the file path and display the image
 file_path=file_name
-# file_path=os.path.join(image_folder_path, file_path_temp)
+
+
 # Step 3 : Preprocess the image 
-#file_path=input("Paste the file path")
 my_image=image.load_img(file_path, target_size=(150, 150))
 my_img_arr=image.img_to_array(my_image)
+my_img_arr=np.expand_dims(my_img_arr, axis=0)
+
 if st.checkbox("Display Image", False):
     image=Image.open(uploaded_file)
     st.image(image)
-my_img_arr=np.expand_dims(my_img_arr, axis=0)
-
-
 # Step4: Get the prediction and print the result
 prediction=int(model.predict(my_img_arr)[0][0])
 if st.button("Predict"):
     if prediction==0:
-        st.write("Its a Cat")
+        st.subheader("Its a Cat")
     if prediction==1:
-        st.write('Its a dog')
+        st.subheader('Its a dog')
 
-# image = tensorflow.keras.utils.load_img(file_path)
-# input_arr = tf.keras.utils.img_to_array(image)
-# input_arr = np.array([input_arr])  # Convert single image to a batch.
-# predictions = model.predict(input_arr)
+
